a523d837a726a3d9427bca9f61f946effb42bbc8
vasquezr8 <vasquezr8@gmail.com>
2023-08-20 19:02:07 -0700

    Still need to convert precipitation to a dictionary

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -8,6 +8,9 @@ from sqlalchemy import create_engine, func
 
 from flask import Flask, jsonify
 
+import pandas as pd
+import datetime as dt
+
 
 #################################################
 # Database Setup
@@ -36,3 +39,38 @@ app = Flask(__name__)
 #################################################
 # Flask Routes
 #################################################
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/start<br/>"
+        f"/api/v1.0/start/end<br/>"
+    )
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return last 12 months of precipitation data"""
+    # Query all passengers
+    year_ago = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+
+    results = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= year_ago).all()
+    
+    session.close()
+    
+    # all_names = [list(r)[0] for r in results]
+
+    # Convert list of tuples into normal list - flattens results
+    last_12_prcp = list(np.ravel(results))
+
+    return jsonify(last_12_prcp)
+
+if __name__ == '__main__':
+    app.run(debug=True)
